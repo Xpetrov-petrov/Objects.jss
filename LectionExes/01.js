ed59ba129087773a98a97ef3167f50eaed6157d8
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-10-26 10:04:36 +0300

    +

---
+++ LectionExes/01.js
@@ -0,0 +1,11 @@
+
+let peter = {
+    name: "Plamen",
+    age: 21,
+    height: 188,
+};
+ printDetails(peter);
+
+ function printDetails(chovek){
+console.log(chovek);
+ }
---
+++ LectionExes/02.js
@@ -0,0 +1,9 @@
+let peter = {
+name: "Plamen",
+age: 21,
+tall: 152,
+};
+console.log(peter);
+
+peter.hairColor = 'red';
+console.log(peter['hairColor']);
