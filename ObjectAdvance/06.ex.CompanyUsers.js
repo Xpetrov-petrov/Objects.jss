0e24b99d3fa4762b970fcca2efb1fbc4175b475e
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-07 17:09:27 +0200

    ++

--- ObjectAdvance/06.ex.CompanyUsers.js
+++ ObjectAdvance/06.ex.CompanyUsers.js
@@ -1,17 +1,31 @@
-function CompanyUsers(arr){
+function CompanyUsers(arr) {
     let companyEmployees = {};
 
-    for(let element of arr){
-       let [company, employeeID] = element.split(' -> ');
-    
+    for (let companyEmployee of arr) {
+        let [company, employeeID] = companyEmployee.split(' -> ');
+
+        if (!(company in companyEmployees)) {
+            companyEmployees[company] = [];
+        }
+
+        if (!companyEmployees[company].includes(employeeID)) {
+            // Ако служителят не е в масива, добавете го
+            companyEmployees[company].push(employeeID);
+        }
     }
-    console.log(companyEmployees);
+
+    let entries = Object.entries(companyEmployees).sort((a, b) => a[0].localeCompare(b[0]));
+
+    for (let [name, employeeIDs] of entries) {
+        console.log(name);
+        employeeIDs.forEach(id => console.log(`-- ${id}`));
+    }
 }
+
 CompanyUsers([
     'SoftUni -> AA12345',
     'SoftUni -> CC12344',
     'Lenovo -> XX23456',
     'SoftUni -> AA12345',
     'Movement -> DD11111'
-    ]);
-
+]);
