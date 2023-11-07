7e98cc00bdc4e97bae95a8bb98d2cc02b20adad2
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-07 19:19:39 +0200

    ++

--- ObjectAdvance/05.ex.CardGame.js
+++ ObjectAdvance/05.ex.CardGame.js
@@ -1,6 +1,6 @@
 function cardGame(arr) {
     let playersCards = {};
-    let array = [];
+    // let array = [];
     let powerValues = {2: 2, 3 :3, 4 :4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, J: 11, Q: 12, K: 13, A: 14};
     let typesValues = { S: 4, H: 3 ,D: 2, C: 1 };
 
