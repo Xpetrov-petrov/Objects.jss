6c36895841abfc383a495e7b3fab77ac6e63722f
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-10 13:11:25 +0200

    ++

--- ObjectAdvance/04.ex.PartyTime.js
+++ ObjectAdvance/04.ex.PartyTime.js
@@ -1,12 +1,51 @@
 function partyTime(arr){
-
-}
-partyTime(['7IK9Yo0h',
-'9NoBUajQ',
-'Ce8vwPmE',
+    function partyTime(input) {
+        let guests = {
+            'VIP': [],
+            'regular': []
+        };
+    
+        let commandIndex = input.indexOf('PARTY');
+    
+        let reservations = input.slice(0, commandIndex);
+    
+        for (let reservation of reservations) {
+            let isVip = /^\d/.test(reservation);
+            if (isVip) {
+                guests['VIP'].push(reservation);
+            } else {
+                guests['regular'].push(reservation);
+            }
+        }
+    
+        let partyGuests = input.slice(commandIndex + 1);
+    
+        for (let guest of partyGuests) {
+            let isVip = /^\d/.test(guest);
+            if (isVip) {
+                let index = guests['VIP'].indexOf(guest);
+                if (index !== -1) {
+                    guests['VIP'].splice(index, 1);
+                }
+            } else {
+                let index = guests['regular'].indexOf(guest);
+                if (index !== -1) {
+                    guests['regular'].splice(index, 1);
+                }
+            }
+        }
+    
+        let totalGuests = guests['VIP'].concat(guests['regular']);
+        
+        console.log(totalGuests.length);
+        console.log(totalGuests.join('\n'));
+    }
+}                                                                   //check tthis after
+partyTime(['Ce8vwPmE',
 'SVQXQCbc',
 'tSzE5t0p',
 'PARTY',
 '9NoBUajQ',
 'Ce8vwPmE',
-'SVQXQCbc'])
+'SVQXQCbc'
+])
--- ObjectAdvance/07.ex.AminerTask.js
+++ ObjectAdvance/07.ex.AminerTask.js
@@ -1,28 +1,26 @@
-function AMinerTask(input) {
+function AMinerTask(arr) {
     let resourses = {};
 
-    for (let i = 0; i < input.length; i += 2) {
-        let resource = input[i];
-        let qty = input[i + 1];
-
-        if (resource in resourses) {   //ако текущият ключ (resource) го има като ключ в обекта (resourses) овеличаваме qty;
-            resourses[resource] += qty;
-
-        } else {  // ако до момента няма такъв ключ в resourses създай със следната стоймост;
+    for(let i = 0; i < arr.length; i += 2){
+        let resource = arr[i];
+        let qty = Number(arr[i+1]);
+        
+        if(resource in resourses){
+              resourses[resource] += qty;
+        }else{
             resourses[resource] = qty;
         }
     }
     let entries = Object.entries(resourses);
-        
-        for(let [resource,qty] of entries){
-          console.log(`${resource} -> ${qty}`);
-        }
-}
+
+    for(let [resource,qty] of entries){
+        console.log(`${resource} -> ${qty}`);
+    }
+}  
 AMinerTask([
     'Gold',
     '155',
     'Silver',
     '10',
     'Copper',
-    '17'
-]);
+    '17']);
