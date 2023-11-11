920174d14aca0223f1578a23cce2bdc1b90fa168
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-11 10:51:48 +0200

    +

---
+++ Text Processing/03.js
@@ -0,0 +1,14 @@
+function solve(text, str){
+let censored = '*'.repeat(str.length); // получаваме брой звездички , колкото е дължината на думата , и после заменяме;
+let result = text;
+
+while(result.indexOf(str)){  // докато текста СЪДЪРЖА str
+    result = result.replace(str, censored) //str го заменяме със censored;
+}
+console.log(result);
+
+
+}
+solve('A small sentence with some words', 'small');
+solve('Find the hidden word', 'hidden' );
+solve('hello world hello', 'hello');
