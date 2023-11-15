31917486c904a7349b07f9a002663661003d758e
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-15 10:34:11 +0200

    +

---
+++ Text Processing Exersize/02.modernTimesOf(#).js
@@ -0,0 +1,23 @@
+function hashTag(text) {
+    words = text.split(' ');
+    let hashWords = words.filter(word => word.startsWith('#') && word.length > 1);
+    // в hashWords остават думите,които са филтрирани така,че да започват с "#" и са с length > 1 за да не хващаме само #зите 
+
+    for (let word of hashWords) {
+        word = word.slice(1);
+        let isValid = true;
+
+        for (let char of word) {
+            if (!/[A-Za-z]/.test(char)) {
+                isValid = false;
+                break; // прекъсва само втория цикъл
+            }
+        }
+
+        if (isValid) {
+            console.log(word);
+        }
+    }
+
+}
+hashTag('Nowadays everyone uses # to tag a #special word in #socialMedia');
