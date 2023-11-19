6146b21b2a46a012c3a71f9bfe305fd9d8acca36
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-19 13:36:05 +0200

    +

---
+++ RegularExpressions/02.ex.js
@@ -0,0 +1,18 @@
+function solve(input){
+let names = input.shift();
+
+let command = input.shift();
+   while(command != 'end of race'){
+
+    
+    command = input.shift();
+   }
+}
+solve(['George, Peter, Bill, Tom',
+'G4e@55or%6g6!68e!!@ ',
+'R1@!3a$y4456@',
+'B5@i@#123ll',
+'G@e54o$r6ge#',
+'7P%et^#e5346r',
+'T$o553m&6',
+'end of race'])
