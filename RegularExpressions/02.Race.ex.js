c5ded2d65fa49021759b60eda7bf1995b3e70ae7
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-22 10:50:18 +0200

    +

--- RegularExpressions/02.Race.ex.js
+++ RegularExpressions/02.Race.ex.js
@@ -3,8 +3,8 @@ function solve(arr) {
     let command = arr.shift();
     let result = {};    //object
 
-    let letterPattern = /[A-Za-z]/g;                    // !!!!
-    let digitPatern = /[\d]/g;
+    let letterPattern = /[A-Za-z+]/g;                    // !!!!
+    let digitPatern = /[\d+]/g;
 
     names.forEach(name => result[name] = 0);
     //въртим през масива names и да зададе всяко име като свойство в обекта result със стойност 0;
@@ -20,14 +20,15 @@ function solve(arr) {
         let name = letterMatches.join('');
 
         if (name in result) {
-            result[name] += distance;
+            result[name] += distance;                                // RegularExpressions/02.Race.ex.js
         }
 
         command = arr.shift();
     }
-    let sortedResult = Object.entries(result).sort((a, b) => b - a);
+    let sortedResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
 
-
+          console.log(sortedResult);
+        //   console.log(Object.entries(result));
     console.log(`1st place: ${sortedResult[0][0]}`);
     console.log(`2nd place: ${sortedResult[1][0]}`);
     console.log(`3rd place: ${sortedResult[2][0]}`);
