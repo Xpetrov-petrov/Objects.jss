08a4a139dc60c90f25b53e63b53216d6e22857b7
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-24 15:07:52 +0200

    с

---
+++ Associative.Arrays/02.js
@@ -0,0 +1,10 @@
+function solve(inpt) {
+    inpt = inpt.split(' ');
+    let object = [];
+
+    for (let word of inpt) {
+        object.push(word);
+    }
+    console.log(object);
+}
+solve('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
