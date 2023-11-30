d5f6bc7268ad7874b20e6cbb7d7f8c4218a5edd2
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-11-30 15:56:10 +0200

    м

---
+++ UPR/1.W.js
@@ -0,0 +1,29 @@
+function solve(arr){
+    let obj1 = {};
+let [firstWord, secondWord] = arr.shift().split(' ');
+let counter1 = 0;
+let counter2 = 0;
+for(let word of arr){
+    if(word == firstWord){
+        counter1++
+        obj1[firstWord] = counter1
+
+    }
+    // if(word == secondWord){
+    //     counter2++
+    //     obj1[secondWord] = counter2
+    // }
+    
+}
+let entries = Object.entries(obj1);
+    entries.sort((a,b) => b[1] - a[1]);
+    for(let [word, count] of entries){
+        console.log(`${word} - ${count}`);
+    }
+}
+solve([
+    'is the',
+    'first', 'sentence', 'Here', 'is',
+    'another', 'the', 'And', 'finally', 'the',
+    'the', 'sentence']
+    )
