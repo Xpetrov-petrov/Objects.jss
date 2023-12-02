7692fd449b6f2e5602ff6c1d87e3d5a1725d0d9c
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-12-02 09:17:04 +0200

    +

---
+++ UPR/111.js
@@ -0,0 +1,45 @@
+function SecretChat(arr){
+    let word = arr.shift();
+
+    while(arr[0] != 'Reveal'){
+       let line = arr.shift();
+       let tokens = line.split(':|:');
+       let command = tokens[0];
+     
+       switch(command){
+
+        case'ChangeAll':
+       let match = tokens[1];
+       let replacement = tokens[2];
+       let parts = word.split(match);
+       word = parts.join(replacement);
+        break;
+
+        case'Reverse':
+       let substring = tokens[1];
+       let firstIndex = word.indexOf(substring);
+       console.log(firstIndex);
+        break;
+
+        case'InsertSpace':
+        break;
+       }
+    }
+}
+SecretChat([
+
+    'Hiware?uiy',
+
+    'ChangeAll:|:i:|:o',
+
+    'Reverse:|:?uoy',
+
+    'Reverse:|:jd',
+
+    'InsertSpace:|:3',
+
+    'InsertSpace:|:7',
+
+    'Reveal'
+
+]);
---
+++ V.FinalTest.exS/LegendaryFarming10.js
@@ -0,0 +1,39 @@
+function legendaryFarming(str) {
+    let specialMatterials = { motes: 0, shards: 0, fragments: 0 };
+    let junkMaterial = {};
+    let SpFrs = {};
+
+    str = str.split(' ');
+    // console.log(str);
+    for (let i = 0; i < str.length; i += 2) {
+        let qty = Number(str[i]);
+        let material = str[i + 1].toLowerCase();
+
+        if (material in specialMatterials) {
+            specialMatterials[material] += qty
+        } else {
+            junkMaterial[material] = qty;
+        }
+        
+        if(specialMatterials[material] >= 250){
+        
+            if(material == 'motes'){
+                specilaFragment = 'Dragonwrath'
+                SpFrs[specialMatterials] = 0
+            }else if(material == 'shards'){
+                specilaFragment = 'Shadowmourne';
+                SpFrs[specialMatterials] = 0
+            }else{
+                specilaFragment = 'Valanyr';
+                SpFrs[specialMatterials] = 0
+            }
+        }else{
+            if(material in junkMaterial){
+               junkMaterial[material] += qty;
+            }else{
+                junkMaterial[material] = qty;
+            }
+    }
+    console.log(`${specilaFragment} obtained!`);
+}
+legendaryFarming('3 Motes 5 stones 5 Shards 6 leathers 255 fragments 7 Shards')
