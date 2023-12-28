af3b8d3eea47d811d328b62912b8c905cc9c945e
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-12-28 12:03:34 +0200

    +

--- common/server.js
+++ common/server.js
@@ -1,10 +1,25 @@
+http.createServer((req, res) => {
+    fs.readFile('./index.html', (err, pageData) => {
+        if (err) {
+            console.error('Грешка при четене на файл:', err);
+            res.writeHead(500);
+            res.end('Вътрешна грешка на сървъра');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(pageData);
+            res.end();
+        }
+    });
+}).listen(3000);
+
+/*
 let http = require('http');
 let fs = require('fs');
 
 
 http.createServer((req,res) => {
-    let pageData = fs.readFileSync('./local.html');
+    let pageData = fs.readFileSync('./index.html');
     res.writeHead(200);
     res.write(pageData);
     res.end();
-}).listen(3000);
+}).listen(3000);*/
